3ce05a84e776dd41000bc192858e9aca544c6159
AndreasStapnes <projstapnes@gmail.com>
2021-10-13 17:07:40 +0200

    Implementerte betydelig raskere versjon

--- functionals.py
+++ functionals.py
@@ -8,43 +8,31 @@ k_b = 1.38e-23
 T = 298
 beta = 1 / (k_b * T)
 
-box_k = k_b*T
+box_k = 3*k_b*T
 box_length = 1
 
 hardcore_diameter = 1e-3
 hardcore_pot = 1000 * k_b * T
 
-jump_scale = 0.1
+jump_scale = 0.04
 
 N = 400
 
 
-class Particle(NamedTuple):
-    x: float
-    y: float
-
-    @classmethod
-    def distance(cls, particle_i: Particle, particle_j: Particle):
-        return np.sqrt((particle_i.x - particle_j.x) ** 2 + (particle_i.y - particle_j.y) ** 2)
-
-    def jump(self):
-        return Particle(self.x + np.random.normal(0, jump_scale),
-                        self.y + np.random.normal(0, jump_scale))
 
 
 class System:
-    state: List[Particle]
+    state: np.ndarray
     particle_amt: int
     dimension: int
-    global_potential: Callable[[Particle], float] = lambda self, particle: 0
-    inter_particle_potential: Callable[[Particle, Particle], float] = lambda self, particle_i, particle_j: 0
-    pressure_method: Callable[[Particle], float] = lambda self, particle: 0
+    L: float
 
-    def __init__(self, state: List[Particle] = None):
-        self.particle_amt = len(state)
-        self.dimension = self.particle_amt * len(state[0])
-        if state is not None:
-            self.state = [Particle(*elem) for elem in state]
+    def __init__(self, particle_amt: int, L: float):
+        self.particle_amt = particle_amt
+        self.positional_dimension = 2
+        self.dimension = self.particle_amt*self.positional_dimension
+        self.state = np.random.random((self.particle_amt, self.positional_dimension)) * L
+        self.L = L
 
     def __len__(self):
         return self.particle_amt
@@ -52,85 +40,36 @@ class System:
     def __getitem__(self, index: int):
         return self.state[index]
 
-    def __abs__(self):
-        amt = len(self)
-        energy = 0.0
-        for i in range(amt):
-            particle_i = self[i]
-            energy += self.global_potential(particle_i)
-            for j in range(i, amt):
-                energy += self.inter_particle_potential(particle_i, self[j])
-        return energy
-
     def jump(self):
-        System2 = System([particle.jump() for particle in sys.state])
-        System2.global_potential = self.global_potential
-        System2.inter_particle_potential = self.inter_particle_potential
-        System2.pressure_method = self.pressure_method
-        return System2
+        next_state = self.state + np.random.normal(0, jump_scale, np.shape(self.state))
+        return next_state
 
-    def next_state(self):
+    def goto_next(self):
         proposed_next = self.jump()
-        delta_energy = abs(proposed_next) - abs(self)
-        if delta_energy < 0:
-            return proposed_next
-        if np.random.random() < np.exp(-beta * delta_energy):
-            return proposed_next
-        else:
-            return self
+        delta_energy = System.energy_calculation(proposed_next, self.L) - self.energy()
+        if delta_energy < 0 or np.random.random() < np.exp(-beta * delta_energy):
+            self.state = proposed_next
 
     def pressure(self) -> float:
-        total_pressure = 0.0
-        for particle in self.state:
-            total_pressure += self.pressure_method(particle)
-        return total_pressure
+        pressure_contrib = np.sum(box_k*self.state*self.L*np.where(self.state < 0, self.state, 0)) \
+            + np.sum(box_k*(self.state-self.L)*np.where(self.state > self.L, self.state, 0))
+        return pressure_contrib
 
+    def energy(self):
+        return System.energy_calculation(self.state, self.L)
 
-
-
-def single_potential(x: float, L: float):
-    if x > L:
-        return 1 / 2 * box_k * (x - L) ** 2
-    elif x < 0:
-        return 1 / 2 * box_k * x ** 2
-    else:
-        return 0
-
-
-def single_force(x: float, L: float):
-    if x > L:
-        return -box_k * (x - L)
-    elif x < 0:
-        return box_k * x
-    else:
-        return 0
-
-
-def box_potential(particle: Particle):
-    x, y = particle.x, particle.y
-    return single_potential(x, box_length) + single_potential(y, box_length)
-
-
-def box_force(particle: Particle, L: float) -> Tuple[float, float]:
-    x, y = particle.x, particle.y
-    return single_force(x, L), single_force(y, L)
-
-
-def single_pressure(particle: Particle):
-    return np.sum(np.abs(np.array(box_force(particle, box_length)) / (4 * box_length)))
-
-
-def hardcore_potential(particle_i: Particle, particle_j: Particle):
-    return hardcore_pot if Particle.distance(particle_i, particle_j) < 2 * hardcore_diameter else 0
+    @classmethod
+    def energy_calculation(self, state:np.ndarray, L:float):
+        energy_contrib = np.sum(1/2*box_k*np.where(state < 0, state, 0)**2) \
+            + np.sum(1/2*box_k*(np.where(state > L, state, 0) - L)**2)
+        return energy_contrib
 
 
 if __name__ == "__main__":
-    sys = System(np.random.random((N, 2)))
-    sys.global_potential = box_potential
-    sys.pressure_method = single_pressure
+    sys = System(N, box_length)
     pressures = []
-    for i in range(3000):
-        sys = sys.next_state()
+    for i in range(100000):
+        sys.goto_next()
         pressures.append(sys.pressure())
     plt.plot(pressures)
     print(f" the average pressure was {np.average(pressures)}")
